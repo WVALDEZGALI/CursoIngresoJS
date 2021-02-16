d0f047631a425f31a4c796e3b9600ae244d7847d
camiloga17310@gmail.com <you@example.com>
2021-02-15 22:58:25 -0300

    cambios

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,18 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	// reservo espacio para guardar el nombre y la edad del usuario.
+	let nombre;
+	let edad;
+	nombre = document.getElementById("txtIdNombre").value;
+	edad = parseFloat(document.getElementById("txtIdEdad").value);
+    /* tomo los datos ingresados por el usuario y los muestro en un alert concatenando 
+	con el caracter "+" entre literales de cadena y variables.*/
+	alert("Usted se llama "+nombre+ " y tiene "+edad+" años")
+	// tambien se puede hacer de esta manera.
+	//alert(`usted se llama ${nombre} y tiene ${edad} años`);
+	document.getElementById("txtIdNombre").value = "";	
+    document.getElementById("txtIdEdad").value = "";
+		
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,23 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	//Declaro las variables numero 1, numero 2 y resultado
+	let numero1;
+	let numero2;
+	let resultado;
+	//Asigno los valores que introduce el usuario en las cajas embebidas. funcion getElementById
+	//numero1 = document.getElementById("txtIdNumeroUno").value;
+	//numero2 = document.getElementById("txtIdNumeroDos").value;
+	//Convierto los valores a numeros enteros con la funcion parseInt(), parseFloat()
+	numero1=parseFloat(document.getElementById("txtIdNumeroUno").value);
+	numero2=parseFloat(document.getElementById("txtIdNumeroDos").value);
+	//Asigno el valor de la variable resultado.
+	resultado=numero1+numero2;
+	//Muestro el resultado de sumar las dos variables en un alert()
+	alert("las sumas es "+resultado);
+	document.getElementById("txtIdNumeroUno").value = "";	
+    document.getElementById("txtIdNumeroDos").value = "";
+
+		
 }
 
